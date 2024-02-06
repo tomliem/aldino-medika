62fa6f21fc78c14355769daaf5113fa76b9e3ebe
tomliem <tom.liem93@gmail.com>
2024-02-06 17:50:43 +0000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,13 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import type { Metadata } from 'next'
 import Footer from '@/components/base/Layout/Footer'
 import Header from '@/components/base/Layout/Header'
-import { ToastContainer } from 'react-toastify';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Aldino FnT',
-  description: 'Freight Forwarder',
+  title: 'Aldino Medika',
+  description: 'One Stop Occupational Health-Safety & Enviromental Services',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -18,18 +17,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <title>Aldino FnT</title>
-        <meta name="description" content="Freight Forwarder" />
+        <title>Aldino Medika</title>
+        <meta name="description" content="One Stop Occupational Health-Safety & Enviromental Services" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className="text-blue-site bg-white">
-        <Header />
         <main className="bg-white min-h-[600px]">{children}</main>
-        <Footer />
-
-        <ToastContainer closeButton={false} pauseOnHover={false} autoClose={3000} position="top-center" />
       </body>
     </html>
   )
